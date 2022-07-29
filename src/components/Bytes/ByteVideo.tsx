cc02cf3c8bc24be77a6e71f53deec407bdff0487
Ram <ramk97095@gmail.com>
2022-07-29 18:00:01 +0530

    fix:icon alignment

--- src/components/Bytes/ByteVideo.tsx
+++ src/components/Bytes/ByteVideo.tsx
@@ -77,7 +77,7 @@ const ByteVideo: FC<Props> = ({ video }) => {
         <TopOverlay playing={playing} onClickPlayPause={onClickVideo} />
         <BottomOverlay video={video} />
         <div ref={observe} />
-        <div className="absolute lg:hidden md:hidden right-2 bottom-20">
+        <div className="absolute lg:hidden md:hidden right-2 top-1/2">
           <ByteActions video={video} />
           {video?.collectModule?.__typename !==
             'RevertCollectModuleSettings' && (
